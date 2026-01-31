379110fdc32d1a83f3d5758b8931922a004a8f09
Jordan Senko <jsenk347@mtroyal.ca>
2026-01-30 21:53:54 -0700

    added some tests for target.py

--- py_code/test_drivers/target_test.py
+++ py_code/test_drivers/target_test.py
@@ -5,10 +5,31 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-from target import AsmInst, AsmInstList, AsmOperand, AsmOperandMode
+from target import AsmInstList, AsmOperand, AsmOperandMode, AsmVariable, AsmRegister
 
-def assert_asm_inst():
-    
+def assert_class_strings():
+    var = AsmVariable("a", 1)
+    assert str(var) == "1", f"ERROR: Variable a = 1 should have string of '1', not {str(var)}"
+
+    reg = AsmRegister(1)
+    assert str(reg) == "R1", f"ERROR: Register 1 should have string of 'R1', not {str(var)}"
+
+    operand = AsmOperand(AsmOperandMode.IMM, 1)
+    assert str(operand) == "1", f"ERROR: Operand literal 1 should have string of '1', not {str(var)}"
+
+    operand.mode = AsmOperandMode.RGD
+    operand.val = reg
+    assert str(operand) == "R1", f"ERROR: Register direct mode operand should have string of 'R1', not {str(var)}"
+
+    operand.mode = AsmOperandMode.ABS
+    operand.val = var
+    assert str(operand) == "1", f"ERROR: Absolute mode operand should have string of '1', not {str(var)}"
+
+    operand.mode = AsmOperandMode.IMM
+    operand.val = 1
+    assert str(operand) == "1", f"ERROR: Immediate mode operand should have string of '1', not {str(var)}"
+
+    print("All type strings pass testing!")
 
 if __name__ == "__main__":
-    pass
+    assert_class_strings()
