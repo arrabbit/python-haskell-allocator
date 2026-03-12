3e1cd79766376f905bb640fa605c5dafa117d0d3
arrabbit <arunningrabbit1@gmail.com>
2026-03-11 20:20:03 -0600

    Created generate.py for assembly conversion

---
+++ py_code/generate.py
@@ -0,0 +1,17 @@
+""" 
+Summary: This handles all logic for generating target assembly code from the
+         intermediate representation.
+
+Authors: Anna Running Rabbit, Jordan Senko, and Joseph Mills
+Date: February 26, 2024
+"""
+from target import AsmInstList, AsmInst, AsmOperator, AsmOperand, AsmOperandMode, AsmRegister, AsmVariable
+
+def generate_assembly(ir_list, colour_map, num_regs):
+    # Initialize assembly instruction list
+    asm_list = AsmInstList(num_regs)
+
+    # TODO loop through IR list and generate assembly instructions based on the
+    # IR instruction type and operands
+
+    return asm_list
