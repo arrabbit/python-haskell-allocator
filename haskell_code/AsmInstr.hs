c87d88022a675819669a22210c02d26cefc5de7e
Jordan Senko <jsenk347@mtroyal.ca>
2026-03-28 16:34:40 -0600

    shows are looking good

--- haskell_code/AsmInstr.hs
+++ haskell_code/AsmInstr.hs
@@ -37,3 +37,49 @@ data AsmInstr
 -- Type defining an AsmProgram as a list of AsmInstr's
 newtype AsmProgram = AsmProgram [AsmInstr]
     deriving (Eq)
+
+------------------
+-- Constructors --
+------------------
+
+mkRegister :: Int -> Register
+mkRegister n
+    | n >= 0    = Register n 
+    | otherwise = error $ "mkRegister: negative register index" ++ show n
+
+
+----------
+-- Show --
+----------
+
+instance Show Register where
+    show :: Register -> String
+    show (Register n) = "R" ++ show n
+
+instance Show SrcOperand where
+    show :: SrcOperand -> String
+    show (ImmSrc n) = "#" ++ show n
+    show (VarSrc v) = v
+    show (RegSrc r) = show r
+
+instance Show DstOperand where
+    show :: DstOperand -> String
+    show (VarDst v) = v
+    show (RegDst r) = show r
+
+instance Show AluOp where
+    show :: AluOp -> String
+    show Add = "ADD"
+    show Sub = "SUB"
+    show Mul = "MUL"
+    show Div = "DIV"
+
+instance Show AsmInstr where
+    show :: AsmInstr -> String
+    show (ArithInstr op src reg)    = show op ++ " " ++ show src ++ "," ++ show reg
+    show (MovFromReg reg dst)       = "MOV " ++ show reg ++ "," ++ show dst
+    show (MovToReg src reg)         = "MOV " ++ show src ++ "," ++ show reg
+
+instance Show AsmProgram where
+    show :: AsmProgram -> String
+    show (AsmProgram instrs) = foldr (\x acc -> x ++ "\n" ++ acc) "" (map show instrs)
