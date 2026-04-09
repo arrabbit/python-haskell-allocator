bdc3ad8915eb7ba588ddff346bc29d211946ee95
arrabbit <arunningrabbit1@gmail.com>
2026-04-08 22:12:39 -0600

    update

--- haskell_code/Allocator.hs
+++ haskell_code/Allocator.hs
@@ -4,6 +4,14 @@
 -- Authors: Anna Running Rabbit, Jordan Senko, and Joseph Mills
 -- Date: April 8, 2026
 
+module Allocator
+    (Colouring
+    , allocate
+    ) where 
+
+import Variable (Variable, getVarName, getAdjacent)
+import InterferenceGraph (IGraph, getVariables)
+
 data Colouring
     = [(varName String, regNum Int)]
 
